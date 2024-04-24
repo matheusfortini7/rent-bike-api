53d94ff4c80bc527e7e07773a9abcc178c8d702b
Matheus Fortini <matheusfortini7@gmail.com>
2024-04-24 08:27:37 -0300

    Passa a deamndar a informação da senha na requisição ao endpoint"

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,8 +1,10 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError; end
       def create
         user = User.find_by(username: params[:username])
+        raise AuthenticationError unless user.authenticate(params.require(:password))
 
         token = AuthenticationTokenService.encode(user.id)
         render json: { token: token }
