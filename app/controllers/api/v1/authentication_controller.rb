c2b5c7e8964a812340ec355f8494ebc6bae22085
Matheus Fortini <matheusfortini7@gmail.com>
2024-04-24 09:02:39 -0300

    Trata os cenários de erro de parâmetro no authenticate

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,12 +2,31 @@ module Api
   module V1
     class AuthenticationController < ApplicationController
       class AuthenticationError < StandardError; end
-      def create
-        user = User.find_by(username: params[:username])
-        raise AuthenticationError unless user.authenticate(params.require(:password))
 
-        token = AuthenticationTokenService.encode(user.id)
-        render json: { token: token }
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticationError, with: :handle_unauthenticated
+
+      def create
+        if user && user.authenticate(params.require(:password))
+          token = AuthenticationTokenService.encode(user.id)
+          render json: { token: token }
+        else
+          raise AuthenticationError
+        end
+      end
+
+      private
+
+      def user
+        @User ||= User.find_by(username: params[:username])
+      end
+
+      def parameter_missing(e)
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        head :unauthorized
       end
     end
   end
